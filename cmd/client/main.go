3ce00a137f1861ecf8596327327bb76556cbc7b0
ogiwara <146042372+ogiogidayo@users.noreply.github.com>
2024-04-17 20:30:33 +0900

    feat: add HelloServerStream

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -3,10 +3,12 @@ package main
 import (
 	"bufio"
 	"context"
+	"errors"
 	"fmt"
 	hellopb "gRPC_Server/pkg/grpc"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
+	"io"
 	"log"
 	"os"
 )
@@ -40,7 +42,8 @@ func main() {
 	client = hellopb.NewGreetingServiceClient(conn)
 	for {
 		fmt.Println("1: send Request")
-		fmt.Println("2: exit")
+		fmt.Println("2: HelloServerStream")
+		fmt.Println("3: exit")
 		fmt.Print("please enter >")
 
 		scanner.Scan()
@@ -49,8 +52,9 @@ func main() {
 		switch in {
 		case "1":
 			Hello()
-
 		case "2":
+			HelloServerStream()
+		case "3":
 			fmt.Println("bye.")
 			goto M
 		}
@@ -69,5 +73,30 @@ func Hello() {
 		fmt.Println(err)
 	} else {
 		fmt.Println(res.GetMessage())
+	}
+}
+
+func HelloServerStream() {
+	fmt.Println("Please enter your name")
+	scanner.Scan()
+	name := scanner.Text()
+
+	req := &hellopb.HelloRequest{Name: name}
+	stream, err := client.HelloServerStream(context.Background(), req)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	for {
+		res, err := stream.Recv()
+		if errors.Is(err, io.EOF) {
+			fmt.Printf("all the responses have already received.")
+			break
+		}
+		if err != nil {
+			fmt.Println(err)
+		}
+		fmt.Println(res)
+
 	}
 }
