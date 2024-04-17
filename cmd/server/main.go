744eb2c70bff6d5a491c49b834cffd9a800d3655
ogiwara <146042372+ogiogidayo@users.noreply.github.com>
2024-04-17 16:18:57 +0900

    feat: add HelloServerStream

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -10,6 +10,7 @@ import (
 	"net"
 	"os"
 	"os/signal"
+	"time"
 )
 
 func NewServer() *gRPCServer {
@@ -47,4 +48,17 @@ func main() {
 
 func (s *gRPCServer) Hello(ctx context.Context, req *hellopb.HelloRequest) (*hellopb.HelloResponse, error) {
 	return &hellopb.HelloResponse{Message: fmt.Sprintf("Hello, %s", req.GetName())}, nil
+}
+
+func (s *gRPCServer) HelloServerStream(req *hellopb.HelloRequest, stream hellopb.GreetingService_HelloServerStreamServer) error {
+	resCount := 5
+	for i := 0; i < resCount; i++ {
+		if err := stream.Send(&hellopb.HelloResponse{
+			Message: fmt.Sprintf("[%d] Hello, %s", i, req.GetName()),
+		}); err != nil {
+			return err
+		}
+		time.Sleep(time.Second * 1)
+	}
+	return nil
 }
